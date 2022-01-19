88a239647ef23355e369fa5b42d0bac06301323d
koleahmantle <koleahmantle@gmail.com>
2022-01-18 21:37:21 -0500

    initial commit, product controller and index view created

---
+++ GeneralStoreMVC/GeneralStoreMVC/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using GeneralStoreMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStoreMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        //Add the application DB context (link to the Database)
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: Product
+        public ActionResult Index()
+        {
+            // See Below (modifying ApplicattionDbContext class)
+            return View(_db.Products.ToList());
+        }
+    }
+}
