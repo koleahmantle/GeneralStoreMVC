c6cae9237f9ed6df9e7108f628602fc2e18252a3
koleahmantle <koleahmantle@gmail.com>
2022-01-19 20:26:20 -0500

    create view completed edit and delete controllers and views added

--- GeneralStoreMVC/GeneralStoreMVC/Controllers/ProductController.cs
+++ GeneralStoreMVC/GeneralStoreMVC/Controllers/ProductController.cs
@@ -1,7 +1,9 @@
 ﻿using GeneralStoreMVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,6 +18,80 @@ namespace GeneralStoreMVC.Controllers
         {
             // See Below (modifying ApplicattionDbContext class)
             return View(_db.Products.ToList());
+        }
+
+        //GET: Product
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Product
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(product);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        //GET: Product/Delete/{id}
+        public ActionResult Delete (int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        ////POST: Product/Delete/{id}
+        //[HttpPost, ActionName ("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id)
+        //{
+        //    //Product product = new _db.Projects.Find(id);
+        //    _db.Products.Remove(product);
+        //    _db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
+        //GET: product/edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        //POST: product/edit/{id}
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(product).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
         }
     }
 }
